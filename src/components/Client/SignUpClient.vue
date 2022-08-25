71c13bbc038d93245ff85264b1dfeb03e2b6a51f
Your Name <you@example.com>
2022-08-25 17:30:00 -0600

    changed minor casing errors

--- src/components/Client/SignUpClient.vue
+++ src/components/Client/SignUpClient.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
-    <h3>Email:<input type="text" name="" id="" /></h3>
-    <h3>First Name:<input type="text" name="" id="" /></h3>
-    <h3>Last Name:<input type="text" name="" id="" /></h3>
-    <h3>img_url:<input type="url" name="" id="" /></h3>
-    <h3>UserName:<input type="text" name="" id="" /></h3>
-    <h3>Password:<input type="text" name="" id="" /></h3>
+    <h3 ref="user_email" class="user_sign_up">
+      Email:<input type="text" name="" id="" />
+    </h3>
+    <h3 ref="user_fname" class="user_sign_up">
+      First Name:<input type="text" name="" id="" />
+    </h3>
+    <h3 ref="user_lname" class="user_sign_up">
+      Last Name:<input type="text" name="" id="" />
+    </h3>
+    <h3 ref="user_img" class="user_sign_up">
+      img_url:<input type="url" name="" id="" />
+    </h3>
+    <h3 ref="user_name" class="user_sign_up">
+      UserName:<input type="text" name="" id="" />
+    </h3>
+    <h3 ref="user_pwd" class="user_sign_up">
+      Password:<input type="text" name="" id="" />
+    </h3>
     <button @click="user_signup">Sign Up</button>
   </div>
 </template>
@@ -14,7 +26,7 @@
 import axios from "axios";
 export default {
   methods: {
-    User_signup() {
+    user_signup() {
       axios
         .request({
           url: `https://innotechfoodie.ml/api/client`,
@@ -24,6 +36,7 @@ export default {
           },
         })
         .then((response) => {
+          this.$refs[`user_sign_up`][`value`]; // seeing if i can do it all as a class if not use refs
           response;
         })
         .catch((error) => {
@@ -31,6 +44,19 @@ export default {
         });
     },
   },
+
+  // mounted() {axios.request({
+  //     url: `https://innotechfoodie.ml/api/client`,
+  //         methods: "POST",
+  //         headers: {
+  //           "https://innotechfoodie.ml/api/client": "yLAQeEigreLkBVEovDJJ",
+  //         },
+  // }).then((response) =>{
+  //         this.$refs[`user_signup`][`value`];
+  //         response;
+  // }).catch((error) => {
+  // error;
+  // }) },
 };
 </script>
 
