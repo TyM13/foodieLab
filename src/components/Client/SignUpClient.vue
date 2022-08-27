3c3b38f58a46b4688e9724df532f7792ef92d78d
Your Name <you@example.com>
2022-08-27 09:26:21 -0600

    foodie update

--- src/components/Client/SignUpClient.vue
+++ src/components/Client/SignUpClient.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <h3 ref="user_email" class="user_sign_up">
-      Email:<input type="text" name="" id="" />
-    </h3>
+    <h3 ref="user_email" class="user_sign_up">Email:<input type="text" /></h3>
     <h3 ref="user_fname" class="user_sign_up">
       First Name:<input type="text" name="" id="" />
     </h3>
@@ -12,51 +10,44 @@
     <h3 ref="user_img" class="user_sign_up">
       img_url:<input type="url" name="" id="" />
     </h3>
-    <h3 ref="user_name" class="user_sign_up">
+    <h3 ref="usr_name" class="user_sign_up">
       UserName:<input type="text" name="" id="" />
     </h3>
     <h3 ref="user_pwd" class="user_sign_up">
       Password:<input type="text" name="" id="" />
     </h3>
-    <button @click="user_signup">Sign Up</button>
+    <button @click="user_signup">sign up</button>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+
 export default {
   methods: {
     user_signup() {
-      axios
-        .request({
-          url: `https://innotechfoodie.ml/api/client`,
-          methods: "POST",
-          headers: {
-            "https://innotechfoodie.ml/api/client": "yLAQeEigreLkBVEovDJJ",
-          },
-        })
-        .then((response) => {
-          this.$refs[`user_sign_up`][`value`]; // seeing if i can do it all as a class if not use refs
-          response;
-        })
-        .catch((error) => {
-          error;
-        });
+        this.$refs(`user_email`)[`value`],
+        this.$refs(`user_fname`)[`value`],
+        this.$refs(`user_lname`)[`value`],
+        this.$refs(`user_img`)[`value`],
+        this.$refs(`usr_name`)[`value`],
+        this.$refs(`user_pwd`)[`value`],
+        axios
+          .request({
+            url: "https://innotechfoodie.ml/api/client",
+            method: "POST",
+            headers: {
+              "x-api-key": "yLAQeEigreLkBVEovDJJ",
+            },
+          })
+          .then((response) => {
+            response;
+          })
+          .catch((error) => {
+            error;
+          });
     },
   },
-
-  // mounted() {axios.request({
-  //     url: `https://innotechfoodie.ml/api/client`,
-  //         methods: "POST",
-  //         headers: {
-  //           "https://innotechfoodie.ml/api/client": "yLAQeEigreLkBVEovDJJ",
-  //         },
-  // }).then((response) =>{
-  //         this.$refs[`user_signup`][`value`];
-  //         response;
-  // }).catch((error) => {
-  // error;
-  // }) },
 };
 </script>
 
---
+++ src/views/RestaurantPublicPage.vue
@@ -0,0 +1,12 @@
+//   mounted() {axios.request({
+//       url: `https://innotechfoodie.ml/api/client`,
+//           methods: "POST",
+//           headers: {
+//             "https://innotechfoodie.ml/api/client": "yLAQeEigreLkBVEovDJJ",
+//           },
+//   }).then((response) =>{
+//           this.$refs[`user_signup`][`value`];
+//           response;
+//   }).catch((error) => {
+//   error;
+//   }) },
